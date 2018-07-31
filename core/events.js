63ff841d50c80666e8dbfd4d9a85ebd56c47d546
ranjayzheng <ranjayzheng@tencent.com>
2018-07-31 17:52:22 +0800

    add some code

---
+++ core/events.js
@@ -0,0 +1,42 @@
+module.exports = {
+  longtap: function() {},
+  dbtap: function(callback, xrange = 100, yrange = 100) {
+    let timer = null
+    let lastClientX
+    let lastClientY
+    return function(e) {
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
+        const thisClientX = e.changedTouches[0].clientX
+        const thisClientY = e.changedTouches[0].clientY
+        x = Math.abs(thisClientX - lastClientX)
+        y = Math.abs(thisClientY - lastClientY)
+        if (x <= xrange && y <= yrange) {
+          e.stopPropagation()
+          callback(e)
+        } else {
+          console.log('Double click in different area!')
+        }
+      } else {
+        lastClientX = e.changedTouches[0].clientX
+        lastClientY = e.changedTouches[0].clientY
+        timer = setTimeout(() => {
+          lastClientX = null
+          lastClientY = null
+          clearTimeout(timer)
+          timer = null
+          console.log('Single click!')
+        }, 500)
+      }
+    }
+  },
+  dragup: function() {},
+  dragdown: function() {},
+  dragright: function() {},
+  dragdown: function() {},
+  swiftup: function() {},
+  swiftdown: function() {},
+  swiftleft: function() {},
+  swiftright: function() {},
+}
---
+++ core/index.js
@@ -0,0 +1,91 @@
+const events = require('./events.js')
+
+class MEvents {
+  /**
+   * mEvents 绑定事件
+   * @param  {String(Selector) || HTMLDivElement}   bindTarget          事件绑定DOM对象
+   * @param  {String(Selector)}   delegateTarget                                    代理对象
+   * @param {String}      event                                                                 待绑定事件
+   * @param  {Function} callback                                                               事件回调
+   * @param  {Object}   data                                                                    传入Handler的数据
+   * @param  {Object}   options                                                                配置参数
+   * {
+   *   isPreventDefault: Boolean,
+   *   isStopPropagation: Boolean
+   * }
+   * @return {HTMLDivElement}                  bindTarget                         事件绑定DOM对象
+   */
+  mEvents(bindTarget, delegateTarget, event, callback, data, options = {
+    isPreventDefault: false,
+    isStopPropagation: false
+  }) {
+    //arrange user input
+    if (typeof delegateTarget === "object") {
+      //mEvents(node, {event: {handler, data, options}})
+      return this._handleEventObj(bindTarget, undefined, delegateTarget)
+    } else if (typeof event === "object") {
+      //mEvents(node, selector, {event: {handler, data, options}})
+      return this._handleEventObj(bindTarget, delegateTarget, event)
+    }
+    const _events = new Set(Object.keys(events))
+    typeof bindTarget === "string" && bindTarget = document.querySelector(bindTarget)
+    if (!_events.has(event)) {
+
+    }
+    events[event](callback, options)
+
+  }
+
+  _delegateEvent(bindTarget, delegateTarget, event, callback) {
+    if (delegateTarget) {
+      const delegateTargets = new Set(document.querySelectorAll(delegateTarget))
+      bindTarget.addEventListener(event, (e) => {
+        target = e.target
+        while (target != bindTarget) {
+          if (delegateTargets.has(target)) {
+            callback(e)
+          } else {
+            target = target.parentNode
+          }
+        }
+      })
+    } else {
+      bindTarget.addEventListener(event, e => {
+
+      })
+    }
+    return bindTarget
+  }
+
+  _handleEventObj(bindTarget, delegateTarget, eventObj) {
+    Object.entries(eventObj).map(bindTargetItem => {
+      if (typeof bindTargetItem[1] === "function") {
+        mEvents(bindTarget, delegateTarget, bindTargetItem[0], bindTargetItem[1])
+      } else {
+        mEvents(bindTarget, delegateTarget, bindTargetItem[0], bindTargetItem[1].handler, bindTargetItem[1].data, bindTargetItem[1].options)
+      }
+    })
+    return bindTarget
+  }
+  /**
+   * getElement 通过选择器获取单个元素
+   * @param  {String} selector H5标准选择器
+   * @return {HTMLDivElement}         选择器捕获的DOM元素
+   */
+  getElement(selector) {
+    return document.querySelector(selector)
+  }
+  /**
+   * getElement 通过选择器获取多个元素
+   * @param  {String} selector H5标准选择器
+   * @return {NodeList}          HTMLDivElement 类数组
+   */
+  getElements(selector) {
+    return document.querySelectorAll(selector)
+  }
+
+}
+
+
+
+module.exports = MEvents
