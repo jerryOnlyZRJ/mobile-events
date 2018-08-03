1512bf1e13ca1927406ffcc2de61252e8e4a2c89
ranjayzheng <ranjayzheng@tencent.com>
2018-08-03 21:33:36 +0800

    add delegate test case

--- test/index.js
+++ test/index.js
@@ -1,13 +1,78 @@
 const mtEvents = require('../lib/index-npm.js')
 
 describe('test MTEvents.bind', () => {
-  test("bind('#bindTarget', 'click', handler) —— 原生事件绑定", () => {
-    document.body.innerHTML = '<div id="bindTarget"></div>'
-    const bindTarget = document.querySelector('#bindTarget')
-    mtEvents(bindTarget, 'click', e => {
-      bindTarget.innerHTML = "click"
-    })
-    bindTarget.click()
-    expect(bindTarget.innerHTML).toBe("click")
-  })
+	test("bind(node, 'click', handler) —— 原生事件绑定", () => {
+		document.body.innerHTML = '<div id="bindTarget"></div>'
+		const bindTarget = document.querySelector('#bindTarget')
+		mtEvents(bindTarget, 'click', e => {
+			bindTarget.innerHTML = "click"
+		})
+		bindTarget.click()
+		expect(bindTarget.innerHTML).toBe("click")
+	})
+	test("bind(node, {'click': handler}) —— 原生事件绑定", () => {
+		document.body.innerHTML = '<div id="bindTarget"></div>'
+		const bindTarget = document.querySelector('#bindTarget')
+		mtEvents(bindTarget, {
+			click: e => {
+				bindTarget.innerHTML = "click"
+			}
+		})
+		bindTarget.click()
+		expect(bindTarget.innerHTML).toBe("click")
+	})
+	test("bind('#bindTarget', 'click', handler) —— 原生事件绑定", () => {
+		document.body.innerHTML = '<div id="bindTarget"></div>'
+		const bindTarget = document.querySelector('#bindTarget')
+		mtEvents('#bindTarget', 'click', e => {
+			bindTarget.innerHTML = "click"
+		})
+		bindTarget.click()
+		expect(bindTarget.innerHTML).toBe("click")
+	})
+})
+
+describe('test delegate event', () => {
+	test("bind('#bindTarget', '#delegateTarget', 'click', handler)", () => {
+		document.body.innerHTML = '<div id="bindTarget"><div id="delegateParent"><div id="delegateTarget"><div id="delegateChild"></div></div></div></div><div id="output"></div>'
+		const bindTarget = document.querySelector('#bindTarget')
+		const delegateParent = document.querySelector('#delegateParent')
+		const delegateTarget = document.querySelector('#delegateTarget')
+		const delegateChild = document.querySelector('#delegateChild')
+		const output = document.querySelector('#output')
+		mtEvents(bindTarget, '#delegateTarget', 'click', e => {
+			output.innerHTML = "delegateTarget click"
+		})
+		bindTarget.click()
+		expect(output.innerHTML).toBe("")
+		delegateParent.click()
+		expect(output.innerHTML).toBe("")
+		delegateTarget.click()
+		expect(output.innerHTML).toBe("delegateTarget click")
+		output.innerHTML = ""
+		delegateChild.click()
+		expect(output.innerHTML).toBe("delegateTarget click")
+	})
+	test("bind('#bindTarget', '#delegateTarget', {'click': handler})", () => {
+		document.body.innerHTML = '<div id="bindTarget"><div id="delegateParent"><div id="delegateTarget"><div id="delegateChild"></div></div></div></div><div id="output"></div>'
+		const bindTarget = document.querySelector('#bindTarget')
+		const delegateParent = document.querySelector('#delegateParent')
+		const delegateTarget = document.querySelector('#delegateTarget')
+		const delegateChild = document.querySelector('#delegateChild')
+		const output = document.querySelector('#output')
+		mtEvents(bindTarget, '#delegateTarget', {
+			click: e => {
+				output.innerHTML = "delegateTarget click"
+			}
+		})
+		bindTarget.click()
+		expect(output.innerHTML).toBe("")
+		delegateParent.click()
+		expect(output.innerHTML).toBe("")
+		delegateTarget.click()
+		expect(output.innerHTML).toBe("delegateTarget click")
+		output.innerHTML = ""
+		delegateChild.click()
+		expect(output.innerHTML).toBe("delegateTarget click")
+	})
 })
