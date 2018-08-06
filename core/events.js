bdc985d114c5df15f1a63a956cb91db619eae2e1
ranjayzheng <ranjayzheng@tencent.com>
2018-08-06 17:27:26 +0800

    rewrite events.js

--- core/events.js
+++ core/events.js
@@ -7,6 +7,126 @@
  * ③delegateTarget：事件代理对象
  */
 class Events {
+  constructor () {
+    /**
+     * longtap 自定义长按事件
+     */
+    this.longtap = {
+      eventHandler: new Map(),
+      bind: (bindTarget, callback, delegateTarget) => {
+        // 可Proxy
+        this.longtap.eventHandler.set(callback, {
+          touchstart: e => {
+            e.preventDefault()
+            const target = this._delegateEvent(
+              bindTarget,
+              delegateTarget,
+              e.target
+            )
+            if ((delegateTarget && target) || !delegateTarget) {
+              timer = setTimeout(() => {
+                clearTimeout(timer)
+                timer = null
+              }, 1000)
+            }
+          },
+          touchend: e => {
+            const target = this._delegateEvent(
+              bindTarget,
+              delegateTarget,
+              e.target
+            )
+            if ((delegateTarget && target) || !delegateTarget) {
+              if (timer) {
+                shortTapCallback && shortTapCallback(e)
+              } else {
+                longTapCallback(e)
+              }
+              clearTimeout(timer)
+              timer = null
+            }
+          }
+        })
+        let longTapCallback = callback
+        let shortTapCallback = null
+        let timer = null
+        if (typeof callback === 'object') {
+          longTapCallback = callback[0]
+          shortTapCallback = callback[1]
+        }
+        bindTarget.addEventListener(
+          'touchstart',
+          this.longtap.eventHandler.get(callback).touchstart
+        )
+        bindTarget.addEventListener(
+          'touchend',
+          this.longtap.eventHandler.get(callback).touchend
+        )
+      },
+      remove (bindTarget, callback) {
+        // 可Proxy
+        Object.entries(this.eventHandler.get(callback)).map(eventItem => {
+          bindTarget.removeEventListener(eventItem[0], eventItem[1])
+        })
+      }
+    }
+    /**
+     * dbtap 自定义双击事件
+     */
+    this.dbtap = {
+      eventHandler: new Map(),
+      bind: (bindTarget, callback, delegateTarget) => {
+        this.dbtap.eventHandler.set(callback, {
+          touchend: e => {
+            const target = this._delegateEvent(
+              bindTarget,
+              delegateTarget,
+              e.target
+            )
+            if ((delegateTarget && target) || !delegateTarget) {
+              if (timer) {
+                clearTimeout(timer)
+                timer = null
+                const thisClientX = e.changedTouches[0].clientX
+                const thisClientY = e.changedTouches[0].clientY
+                const x = Math.abs(thisClientX - lastClientX)
+                const y = Math.abs(thisClientY - lastClientY)
+                if (x <= xrange && y <= yrange) {
+                  e.preventDefault()
+                  callback(e)
+                } else {
+                  console.log('Double click in different area!')
+                }
+              } else {
+                lastClientX = e.changedTouches[0].clientX
+                lastClientY = e.changedTouches[0].clientY
+                timer = setTimeout(() => {
+                  lastClientX = null
+                  lastClientY = null
+                  timer && clearTimeout(timer)
+                  timer = null
+                }, 500)
+              }
+            }
+          }
+        })
+        const xrange = 100
+        const yrange = 100
+        let timer = null
+        let lastClientX
+        let lastClientY
+        bindTarget.addEventListener(
+          'touchend',
+          this.dbtap.eventHandler.get(callback).touchend
+        )
+      },
+      remove (bindTarget, callback) {
+        Object.entries(this.eventHandler.get(callback)).map(eventItem => {
+          bindTarget.removeEventListener(eventItem[0], eventItem[1])
+        })
+      }
+    }
+  }
   /**
    * _delegateEvent 事件代理处理
    * @param  {String(Selector) | HTMLDivElement} bindTarget     事件绑定元素
@@ -25,79 +145,6 @@ class Events {
       }
     }
     return null
-  }
-  /**
-   * longtap 自定义长按事件
-   */
-  longtap (bindTarget, callback, delegateTarget) {
-    let longTapCallback = callback
-    let shortTapCallback = null
-    let timer = null
-    if (typeof callback === 'object') {
-      longTapCallback = callback[0]
-      shortTapCallback = callback[1]
-    }
-    bindTarget.addEventListener('touchstart', e => {
-      e.preventDefault()
-      const target = this._delegateEvent(bindTarget, delegateTarget, e.target)
-      if ((delegateTarget && target) || !delegateTarget) {
-        timer = setTimeout(() => {
-          clearTimeout(timer)
-          timer = null
-        }, 1000)
-      }
-    })
-    bindTarget.addEventListener('touchend', e => {
-      const target = this._delegateEvent(bindTarget, delegateTarget, e.target)
-      if ((delegateTarget && target) || !delegateTarget) {
-        if (timer) {
-          shortTapCallback && shortTapCallback(e)
-        } else {
-          longTapCallback(e)
-        }
-        clearTimeout(timer)
-        timer = null
-      } else {
-      }
-    })
-  }
-  /**
-   * dbtap 自定义双击事件
-   */
-  dbtap (bindTarget, callback, delegateTarget) {
-    const xrange = 100
-    const yrange = 100
-    let timer = null
-    let lastClientX
-    let lastClientY
-    bindTarget.addEventListener('touchend', e => {
-      const target = this._delegateEvent(bindTarget, delegateTarget, e.target)
-      if ((delegateTarget && target) || !delegateTarget) {
-        if (timer) {
-          clearTimeout(timer)
-          timer = null
-          const thisClientX = e.changedTouches[0].clientX
-          const thisClientY = e.changedTouches[0].clientY
-          const x = Math.abs(thisClientX - lastClientX)
-          const y = Math.abs(thisClientY - lastClientY)
-          if (x <= xrange && y <= yrange) {
-            e.preventDefault()
-            callback(e)
-          } else {
-            console.log('Double click in different area!')
-          }
-        } else {
-          lastClientX = e.changedTouches[0].clientX
-          lastClientY = e.changedTouches[0].clientY
-          timer = setTimeout(() => {
-            lastClientX = null
-            lastClientY = null
-            timer && clearTimeout(timer)
-            timer = null
-          }, 500)
-        }
-      }
-    })
   }
 }
 
