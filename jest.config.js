6acf256f8c19268a8978b88f39b0bb648506cc86
ranjayzheng <ranjayzheng@tencent.com>
2018-08-21 22:18:34 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@ module.exports = {
   testURL: 'http://localhost',
   testMatch: ['<rootDir>/test/*.js'],
   coverageDirectory: '<rootDir>/test/coverage',
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 90,
-  //     functions: 90,
-  //     lines: 90,
-  //     statements: 90
-  //   }
-  // }
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
 }
