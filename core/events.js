db5ff175628a86ee5eae0e284686a7a04d28c4b9
ranjayzheng <ranjayzheng@tencent.com>
2018-08-22 12:31:03 +0800

    add function getVectorLength

--- core/events.js
+++ core/events.js
@@ -1,6 +1,10 @@
 const SingleEvent = require('./tools/singleevent.js')
 const Timer = require('./tools/timer.js')
 const Position = require('./tools/position.js')
+
+function getVectorLength (x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+}
 /**
  * _arrangeCallbackArr 处理用户传入回调数组
  * @param  {Object} e             原生事件对象
@@ -185,16 +189,20 @@ class Events {
             e.preventDefault()
             if (e.touches.length === 2) {
               if (!lastSpaceOf2Touch) {
-                lastSpaceOf2Touch = Math.sqrt(
-                  Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2) +
-                    Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2)
+                lastSpaceOf2Touch = getVectorLength(
+                  e.touches[0].clientX,
+                  e.touches[0].clientY,
+                  e.touches[1].clientX,
+                  e.touches[1].clientY
                 )
               } else if (!timer.timer) {
                 timer.timeoutCreator(50, () => {
                   if (!lastSpaceOf2Touch) return
-                  const thisSpaceOf2Touch = Math.sqrt(
-                    Math.pow(e.touches[0].clientX - e.touches[1].clientX, 2) +
-                      Math.pow(e.touches[0].clientY - e.touches[1].clientY, 2)
+                  const thisSpaceOf2Touch = getVectorLength(
+                    e.touches[0].clientX,
+                    e.touches[0].clientY,
+                    e.touches[1].clientX,
+                    e.touches[1].clientY
                   )
                   // matrix(0.75, 0, 0, 0.75, 0, 0)
                   const scale =
@@ -232,11 +240,17 @@ class Events {
                   const lastY2 = lastArrOf2Touch[1].clientY
                   const thisY1 = e.touches[0].clientY
                   const thisY2 = e.touches[1].clientY
-                  const lengthOfLast = Math.sqrt(
-                    Math.pow(lastX2 - lastX1, 2) + Math.pow(lastY2 - lastY1, 2)
+                  const lengthOfLast = getVectorLength(
+                    lastX1,
+                    lastY1,
+                    lastX2,
+                    lastY2
                   )
-                  const lengthOfThis = Math.sqrt(
-                    Math.pow(thisX2 - thisX1, 2) + Math.pow(thisY2 - thisY1, 2)
+                  const lengthOfThis = getVectorLength(
+                    thisX1,
+                    thisY1,
+                    thisX2,
+                    thisY2
                   )
                   const rotateAngle =
                     (Math.acos(
